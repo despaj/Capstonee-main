94ecb780eb526e23355f1bf38a1500b9e11c1cf5
despaj <despajanelle@gmail.com>
2026-04-14 02:38:36 +0800

    Update server.js

--- ifranchise-backend/server.js
+++ ifranchise-backend/server.js
@@ -34,6 +34,8 @@
   const otpStore = {};
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    port: 587,
+  secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
